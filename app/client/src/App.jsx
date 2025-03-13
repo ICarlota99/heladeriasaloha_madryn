4f4766f424a16b2f2c7cf1b3eb9b24874d585d67
ICarlota99 <k431999@gmail.com>
2025-03-13 12:38:03 -0400

    add sections to app

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,21 +1,29 @@
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
-import About from "./sections/About";
-import Whyus from "./sections/Whyus";
 import Shop from "./sections/Shop";
-import Contact from "./sections/Contact";
-import Footer from "./sections/Footer";
+import Flavors from "./sections/Flavors";
+import Whyus from "./sections/Whyus";
+import Delivery from "./sections/Delivery";
+import WhatsAppButton from "./components/WhatsappButton";
+// import About from "./sections/About";
+// import Contact from "./sections/Contact";
+// import Footer from "./sections/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <About />
-      <Whyus />
       <Shop />
+      <Flavors />
+      <Whyus />
+      <Delivery />
+      <WhatsAppButton />
+      {/* <About />
+
+
       <Contact />
-      <Footer />
+      <Footer /> */}
     </>
   )
 }
