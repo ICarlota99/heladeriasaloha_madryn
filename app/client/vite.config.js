473cb1c2cb8189b68b1602588fa2018e4fc587dd
ICarlota99 <k431999@gmail.com>
2025-03-17 16:26:57 -0400

    minor refactoring

--- app/client/vite.config.js
+++ app/client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+// import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
 import { fileURLToPath } from 'url';
 // import svgr from "vite-plugin-svgr";
@@ -15,7 +15,7 @@ export default defineConfig({
     // svgr({
     //   include: "**/*.svg?react"
     // }),
-    tailwindcss(),
+    // tailwindcss(),
   ],
   resolve: {
     dedupe: ['react', 'react-dom'], // Prevent duplicate React instances
