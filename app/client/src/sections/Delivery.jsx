5f2157f1b8d8cbc746c3638d5217ae5c5b19de57
ICarlota99 <k431999@gmail.com>
2025-03-17 10:42:06 -0400

    add: delivery section

--- app/client/src/sections/Delivery.jsx
+++ app/client/src/sections/Delivery.jsx
@@ -4,7 +4,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 const Delivery = () => {
-  const [customDiv, setCustomDiv] = useState({ position: 'absolute', top: '10%', right: '20%', width: '35%' }); // Default div styles
+  const [customDiv, setCustomDiv] = useState({ position: 'absolute', top: '10%', right: '5%', width: '50%' }); // Default div styles
   const [windowWidth, setWindowWidth] = useState(window.innerWidth); // Initial width
   
   useEffect(() => {
@@ -26,62 +26,66 @@ const Delivery = () => {
 
   useEffect(() => {
     // Determine window width
-    if (windowWidth < 768) {
-      // Small screens
+    if (windowWidth < 900) {
+      // Small and medium screens
       setCustomDiv({ width: '100%' });
-    } else if (windowWidth >= 768 && windowWidth < 992) {
-      // Medium screens
-      setCustomDiv({ width: '100%' });
+    // } else if (windowWidth >= 900 && windowWidth < 992) {
+    //   // Medium screens
+    //   setCustomDiv({ width: '100%' });
+    // } else if (windowWidth >= 900 && windowWidth < 1024) {
+    //   // Large screens
+    //   setCustomDiv({ width: '50%' });
     } else {
-      // Large screens
-      setCustomDiv({ position: 'absolute', top: '10%', right: '20%', width: '35%' });
+      // XL screens
+      setCustomDiv({ width: '50%' });
     }
   }, [windowWidth]);
 
   return (
     <div
       id="delivery"
-      className="d-flex flex-md-row flex-lg-row flex-sm-column container-fluid position-relative"
+      className="container-fluid position-relative my-4"
     >
       {/* Image on the left */}
       <div className="position-relative">
         <img
         id='delivery-img'
-          data-aos="slide-right"
-          data-aos-delay="200"
+          data-aos="slide-left"
+          data-aos-delay="300"
           src={frame}
           alt="shop image"
-          className="w-50"
-          style={{ zIndex: -1 }}
+          className="position-absolute"
+          style={{ zIndex: -1, top: '0%', right: '0%', width: '60%' }}
         />
       </div>
 
       <div
-        className="d-flex flex-column content-container"
-        style={customDiv}
-      >
+        className="d-flex flex-column mt-5">
         <div
-          data-aos="fade-up-right"
+          data-aos="slide-down"
           data-aos-delay="400"
-          className="dark-container py-4 px-4 mb-4"
+          className="dark-container py-5 px-3 mt-5"
+          style={customDiv}
         >
-          <h1 className="font-lobster text-center py-2">Tienda Online</h1>
-          <p className="text-center fs-3 mx-4">DELIVERY EN TODO MADRYN</p>
+          <h1 className="font-lobster text-center pt-5">🛒 Tienda Online 🚙</h1>
+          <p className="text-center fs-3 mx-4 pb-5">DELIVERY EN TODO MADRYN</p>
         </div>
 
         <div
-          data-aos="fade-up-left"
+          data-aos="slide-up"
           data-aos-delay="400"
-          className="px-5 py-4 mt-4 bg-white"
+          className="px-4 py-4 mt-4 bg-white text-center"
+          style={customDiv}
         >
-          <h1 className="font-lobster text-center py-2">Helados & Postres</h1>
+          <h1 className="font-lobster py-2">Helados & Postres</h1>
           <p className="lh-lg">
-            Nuestros helados se elaboran con ingredientes de 
-            primera calidad. No te quedes con las ganas. <br />
-            Pedi
-            delivery y disfruta de una experiencia inolvidable.
-          </p>
-          <button className="btn btn-lg btn-dark hvr-grow-shadow">
+            Queremos endulzar cada momento de tu vida con nuestras cremas heladas, 
+            tortas, postres, alfajores y palitos helados que deleitan el paladar. 
+            No te quedes con las ganas.          
+            </p>
+            <h4 className='py-4'>Pedi <span><strong>DELIVERY</strong></span> 🚚 <br />
+            y disfruta de una experiencia inolvidable.</h4>
+          <button className="btn btn-lg btn-dark hvr-grow-shadow ">
             <a href="">CONTÁCTANOS</a>
           </button>
         </div>
