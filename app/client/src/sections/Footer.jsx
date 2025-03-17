8e2660ba702e207b6e4c1168c2f1d89bd28b8827
ICarlota99 <k431999@gmail.com>
2025-03-17 10:43:15 -0400

    add: footer section

--- app/client/src/sections/Footer.jsx
+++ app/client/src/sections/Footer.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div 
+      className='dark-section container-fluid text-center py-2 z-1'
+      style={{backgroundSize: '100% 10000px'}}>
+      <p className="copyright">©2024 ALOSURMORA.SA Todos los derechos reservados</p>
+      <p>Diseñado y desarrollado por <a className='light-link' href="">ICarlota99</a></p>
     </div>
   )
 }
