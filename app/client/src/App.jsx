d46e5c3b26da4ef7bd2bd726cafc6b680b5dd9c2
ICarlota99 <k431999@gmail.com>
2025-03-18 19:04:01 -0400

    clean App structure

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./sections/Header";
 import Home from "./pages/Home";
 import About from './pages/About';
+import Products from './pages/Products';
 // import Products from './pages/Products';
 // import Contact from './pages/Contact';
 import Footer from "./sections/Footer";
@@ -14,16 +15,12 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/category/:category" element={<Products />} />
         {/* <Route path="/products" element={<Products />} /> */}
         {/* <Route path="/contact" element={<Contact />} /> */}
       </Routes>
       <Footer />
       <WhatsAppButton />
     </Router>
-    // <>
-    //   <Header />
-    //   <Home />
-    //   <WhatsAppButton />
-    // </>
   )
 }
