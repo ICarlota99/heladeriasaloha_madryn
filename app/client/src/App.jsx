eeddf569144f2712810c15684e85e1572f082069
ICarlota99 <k431999@gmail.com>
2025-03-24 22:09:26 -0400

    feat(cart): add cart provider to the main app

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -3,24 +3,23 @@ import Header from "./sections/Header";
 import Home from "./pages/Home";
 import About from './pages/About';
 import Products from './pages/Products';
-// import Products from './pages/Products';
-// import Contact from './pages/Contact';
 import Footer from "./sections/Footer";
 import WhatsAppButton from "./components/WhatsappButton";
+import { CartProvider } from './components/CartContext';
 
 export default function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/category/:category" element={<Products />} />
-        {/* <Route path="/products" element={<Products />} /> */}
-        {/* <Route path="/contact" element={<Contact />} /> */}
-      </Routes>
-      <Footer />
-      <WhatsAppButton />
-    </Router>
+    <CartProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/category/:category" element={<Products />} />
+        </Routes>
+        <Footer />
+        <WhatsAppButton />
+      </Router>
+    </CartProvider>
   )
 }
