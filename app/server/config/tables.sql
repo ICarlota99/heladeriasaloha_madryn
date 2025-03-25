375b8b6bf3d45364bb3f8f736169077e1f175097
ICarlota99 <k431999@gmail.com>
2025-03-24 22:16:18 -0400

    add: new table unverified_users, improve users table

--- app/server/config/tables.sql
+++ app/server/config/tables.sql
@@ -1,8 +1,24 @@
+CREATE TYPE user_role AS ENUM ('owner', 'worker', 'customer');
+
 CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(120) NOT NULL,
-        email VARCHAR(120) NOT NULL UNIQUE,
-        pw_hash VARCHAR(120) NOT NULL
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(120) NOT NULL,
+    email VARCHAR(120) UNIQUE,
+    phone VARCHAR(15) UNIQUE,
+    pw_hash VARCHAR(120) NOT NULL,
+    role user_role NOT NULL DEFAULT 'customer',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE unverified_users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(120) NOT NULL,
+    email VARCHAR(120),
+    phone VARCHAR(15),
+    pw_hash VARCHAR(120) NOT NULL,
+    verification_token VARCHAR(120) NOT NULL,
+    token_expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE categories (
