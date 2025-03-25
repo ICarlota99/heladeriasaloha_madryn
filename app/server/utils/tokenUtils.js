5cf50981a9047afd98c3fd5f2b170b474312d517
ICarlota99 <k431999@gmail.com>
2025-03-25 11:42:08 -0400

    improve token generation

--- app/server/utils/tokenUtils.js
+++ app/server/utils/tokenUtils.js
@@ -1,7 +1,17 @@
-import crypto from 'crypto';
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
-const generateVerificationToken = () => {
-    return crypto.randomBytes(20).toString('hex'); // Generate a random token
-};
+function generateToken(userId, purpose) {
+  return jwt.sign(
+    {
+      sub: userId,
+      purpose: purpose,
+      jti: crypto.randomBytes(16).toString("hex"),
+      iat: Math.floor(Date.now() / 1000),
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+}
 
-export default generateVerificationToken;
+export default generateToken;
