75b3846a9c5dd7487a9896dd28b675a69720cfeb
ICarlota99 <k431999@gmail.com>
2025-03-27 00:02:15 -0400

    delete: backend routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
-import { AuthProvider } from './context/AuthContext';
 import { ToastContainer } from 'react-toastify';
 import CartPage from './pages/Cart';
 import CheckoutPage from './pages/Checkout'
-import AuthForm from './pages/Auth';
-import VerifyEmail from './pages/VerifyEmail';
+import Sabores from './pages/Sabores';
 import Header from './sections/Header';
 import HomePage from "./pages/Home";
 import About from './pages/About';
@@ -15,24 +13,21 @@ import WhatsAppButton from './components/WhatsappButton';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <Router>
-          <Header />
-          <ToastContainer position='top-right' autoClose={5000} />
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/category/:category' element={<Products />} />
-            <Route path='/cart' element={<CartPage />} /> 
-            <Route path='/checkout' element={<CheckoutPage />} />
-            <Route path='/login' element={<AuthForm />} />
-            <Route path='/verify' element={<VerifyEmail />} />
-          </Routes>
-          <Footer />
-          <WhatsAppButton />
-        </Router>
-      </CartProvider>
-    </AuthProvider>
+    <CartProvider>
+      <Router>
+        <Header />
+        <ToastContainer position='top-right' autoClose={5000} />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/category/:category' element={<Products />} />
+          <Route path='/cart' element={<CartPage />} /> 
+          <Route path='/checkout' element={<CheckoutPage />} />
+          <Route path='/flavors' element={<Sabores />} />
+        </Routes>
+        <Footer />
+        <WhatsAppButton />
+      </Router>
+    </CartProvider>
   )
 }
